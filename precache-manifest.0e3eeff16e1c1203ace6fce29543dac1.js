1db9677ea2ad723672adbb5086f1937694151619
Lennon Choong <lennonchoong@Lennons-MacBook-Air.local>
2020-10-16 12:16:10 +0800

    Updates

---
+++ precache-manifest.0e3eeff16e1c1203ace6fce29543dac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0092492865e5035aa3755699fb625d",
+    "url": "/AXA-react-app/index.html"
+  },
+  {
+    "revision": "7b101565853faf0d90ed",
+    "url": "/AXA-react-app/static/css/main.a220b4ef.chunk.css"
+  },
+  {
+    "revision": "499b5aa3e460ed961392",
+    "url": "/AXA-react-app/static/js/2.0014ef0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AXA-react-app/static/js/2.0014ef0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b101565853faf0d90ed",
+    "url": "/AXA-react-app/static/js/main.dfd26678.chunk.js"
+  },
+  {
+    "revision": "86fb7e1f90e529384706",
+    "url": "/AXA-react-app/static/js/runtime-main.1a09a4c2.js"
+  },
+  {
+    "revision": "7ed5f0cb88d5ae06791e05cf23e84c1f",
+    "url": "/AXA-react-app/static/media/kelly-sikkema-WvVyudMd1Es-unsplash.7ed5f0cb.jpg"
+  }
+]);
