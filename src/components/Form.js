4cf4c74424ea166aa1ca889c8e3d50720b402bce
Lennon Choong <lennonchoong@Lennons-MacBook-Air.local>
2020-10-18 00:03:22 +0800

    added forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import BlobSVGWrapper from "./formcomponents/BlobSVGWrapper";
 
 class Form extends Component {
     constructor(props) {
         super(props);
         this.curRef = props.curRef;
-        this.updateNationality = this.updateNationality.bind(this);
+        this.updateState = this.updateState.bind(this);
         this.incrementPage = this.incrementPage.bind(this);
         this.decrementPage = this.decrementPage.bind(this);
         this.state = {
-            count: 0,
-        }
+            count: 0
+        };
     }
 
-    updateNationality(val) {
-        this.setState({
-            malaysian: val
-        });
+    updateState(obj) {
+        this.setState(obj);
     }
 
     incrementPage() {
-        this.setState((prevState) => ({
+        this.setState(prevState => ({
             count: prevState.count + 1
-        }))
+        }));
     }
 
     decrementPage() {
         if (this.state.count > 0) {
-            this.setState((prevState) => ({
+            this.setState(prevState => ({
                 count: prevState.count - 1
-            }))
+            }));
         }
     }
 
@@ -37,26 +36,43 @@ class Form extends Component {
         let backButton;
 
         if (this.state.count === 0) {
-            curPage =  <Nationality updateNationality={this.updateNationality} incrementPage={this.incrementPage}/>
+            curPage = (
+                <Nationality
+                    updateState={this.updateState}
+                    incrementPage={this.incrementPage}
+                />
+            );
+        } else if (this.state.count === 1 && this.state.malaysian === true) {
+            curPage = (
+                <IndividualOrFamilyPlan
+                    updateState={this.updateState}
+                    incrementPage={this.incrementPage}
+                />
+            );
         }
 
         if (this.state.count > 0) {
-            backButton = <BackButton decrementPage={this.decrementPage}/>
+            backButton = <BackButton decrementPage={this.decrementPage} />;
         }
 
         return (
-            <div className="form" onClick={() => console.log(this.state)} ref={this.curRef}>
+            <div
+                className="form"
+                onClick={() => console.log(this.state)}
+                ref={this.curRef}
+            >
                 {backButton}
                 {curPage}
+                <BlobSVGWrapper />
             </div>
-        )
+        );
     }
 }
 
 class Nationality extends Component {
     constructor(props) {
         super(props);
-        this.updateNationality = props.updateNationality;
+        this.updateState = props.updateState;
         this.incrementPage = props.incrementPage;
     }
 
@@ -64,12 +80,84 @@ class Nationality extends Component {
         return (
             <div className="nationality">
                 <span>Are you a Malaysian?</span>
-                <div>
-                    <FormButtons text="Yes" incrementPage={this.incrementPage} updateNationality={this.updateNationality}/>
-                    <FormButtons text="No" incrementPage={this.incrementPage} updateNationality={this.updateNationality}/>
-                </div>
+                <TwoButtons
+                    incrementPage={this.incrementPage}
+                    updateState={this.updateState}
+                    context="malaysian"
+                />
             </div>
-        )
+        );
+    }
+}
+
+class IndividualOrFamilyPlan extends Component {
+    constructor(props) {
+        super(props);
+        this.updateState = props.updateState;
+        this.incrementPage = props.incrementPage;
+    }
+
+    render() {
+        return (
+            <div className="plan-type">
+                <span>Individual or Family Plan?</span>
+                <TwoButtons
+                    incrementPage={this.incrementPage}
+                    updateState={this.updateState}
+                    context="plan-type"
+                />
+            </div>
+        );
+    }
+}
+
+class TwoButtons extends Component {
+    constructor(props) {
+        super(props);
+        this.updateState = props.updateState;
+        this.incrementPage = props.incrementPage;
+    }
+
+    render() {
+        if (this.props.context === "malaysian") {
+            return (
+                <div className="btn-group">
+                    <FormButtons
+                        className="left"
+                        text="yes"
+                        incrementPage={this.incrementPage}
+                        updateState={this.updateState}
+                        context={this.props.context}
+                    />
+                    <FormButtons
+                        className="right"
+                        text="no"
+                        incrementPage={this.incrementPage}
+                        updateState={this.updateState}
+                        context={this.props.context}
+                    />
+                </div>
+            );
+        } else if (this.props.context === "plan-type") {
+            return (
+                <div className="btn-group">
+                    <FormButtons
+                        className="left"
+                        text="individual"
+                        incrementPage={this.incrementPage}
+                        updateState={this.updateState}
+                        context={this.props.context}
+                    />
+                    <FormButtons
+                        className="right"
+                        text="family"
+                        incrementPage={this.incrementPage}
+                        updateState={this.updateState}
+                        context={this.props.context}
+                    />
+                </div>
+            );
+        }
     }
 }
 
@@ -77,19 +165,37 @@ class FormButtons extends Component {
     constructor(props) {
         super(props);
         this.value = props.text;
-        this.updateNationality = props.updateNationality;
+        this.updateState = props.updateState;
         this.incrementPage = props.incrementPage;
     }
 
     render() {
+        let obj;
+
+        if (this.props.context === "malaysian") {
+            obj = {
+                [`${this.props.context}`]: this.value === "yes" ? true : false
+            };
+        } else if (this.props.context === "plan-type") {
+            obj = {
+                [`${this.props.context}`]:
+                    this.value === "family" ? "family" : "individual"
+            };
+        }
+
         return (
-            <div className="form-btn" onClick={() => {
-                this.updateNationality(this.value === "Yes" ? true : false);
-                this.incrementPage();
-                }}>
-                {this.value}
+            <div
+                className={`form-btn ${this.props.className}`}
+                onClick={() => {
+                    this.updateState(obj);
+                    this.incrementPage();
+                }}
+            >
+                <span>
+                    {this.value.charAt(0).toUpperCase() + this.value.slice(1)}
+                </span>
             </div>
-        )
+        );
     }
 }
 
@@ -101,13 +207,13 @@ class BackButton extends Component {
 
     render() {
         return (
-            <div onClick={() => this.decrementPage()}className="back-btn">
-                back
+            <div onClick={() => this.decrementPage()} className="back-btn">
+                <svg viewBox="0 0 1125 1125" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M367.438 178.881C382.764 163.019 408.272 163.019 424.135 178.881C439.461 194.207 439.461 219.715 424.135 235.006L136.65 522.49H1085.31C1107.42 522.526 1125 540.103 1125 562.217C1125 584.331 1107.42 602.48 1085.31 602.48H136.65L424.135 889.428C439.461 905.29 439.461 930.834 424.135 946.124C408.272 961.987 382.729 961.987 367.438 946.124L11.8966 590.583C-3.96555 575.257 -3.96555 549.749 11.8966 534.458L367.438 178.881Z" fill="#F8F8F8"/>
+                </svg>
             </div>
-        )
+        );
     }
 }
-
-
 
 export default Form;
